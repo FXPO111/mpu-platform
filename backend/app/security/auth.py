0006fcb715769625e22457d2b0d601def99ff55f
FXPO111 <td4434198@gmail.com>
2026-02-17 16:29:44 +0200

    Update auth.py

--- backend/app/security/auth.py
+++ backend/app/security/auth.py
@@ -5,8 +5,10 @@ from passlib.context import CryptContext
 
 from app.settings import settings
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
+pwd_context = CryptContext(
+    schemes=["bcrypt_sha256", "bcrypt"],
+    deprecated="auto",
+)
 
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
