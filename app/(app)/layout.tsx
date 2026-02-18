b009ee885efbe10a11df4937fc8fe7fc9ec6692f
FXPO111 <td4434198@gmail.com>
2026-02-18 04:41:21 +0200

    Delete app directory

--- app/(app)/layout.tsx
+++
@@ -1,6 +0,0 @@
-import type { ReactNode } from "react";
-import AppLayout from "@/components/layout/AppLayout";
-
-export default function CabinetLayout({ children }: { children: ReactNode }) {
-  return <AppLayout>{children}</AppLayout>;
-}
